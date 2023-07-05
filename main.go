f98489b465966534bf442d39a64fc3435e34a722
Stephen Kelly <stevekm@users.noreply.github.com>
2023-07-05 15:45:24 -0400

    fix coordinate covert method

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func loadQueries(filename string) ([]Query, error) {
 }
 
 func ConvertCoordinate(query Query, transcripts map[string]Transcript) (string, int, error) {
-	fmt.Printf("query: %v, transcripts: %v\n", query, transcripts)
+	// fmt.Printf("query: %v, transcripts: %v\n", query, transcripts)
 	// check if the Query transcript name is in the transcript map
 	_, ok := transcripts[query.Name]
 	// If the key does not exist, return error
@@ -97,21 +97,27 @@ func ConvertCoordinate(query Query, transcripts map[string]Transcript) (string, 
 	// get the transcript map entry
 	transcript, _ := transcripts[query.Name]
 
+	// this is the value we will be returning
+	// this value will get adjusted based on the next calculations
+	finalPos := transcript.Pos // genomic_pos
+	// this value can also get adjusted based on the next calculations
+	queryCoordinate := query.Pos // transcript_query_coord
+
 	// cigar_string := transcript.Cigar // "8M7D6M2I2M11D7M"
 	re := regexp.MustCompile(`(\d+)([MDISHX])`)
 	matches := re.FindAllStringSubmatch(transcript.Cigar, -1) // [ [8M 8 M] , [7D 7 D], [6M 6 M], ... ]
 	for _, match := range matches {                           // [8M 8 M]
 		// convert the position match value to int
-		cigarPos, err := strconv.Atoi(match[1])
+		cigarPos, err := strconv.Atoi(match[1]) // cigar_op_length
 		if err != nil {
 			log.Fatal(err)
 		}
 		cigarOperation := match[2]
-		fmt.Printf("%v, %v\n", cigarPos, cigarOperation)
+		// fmt.Printf("%v, %v\n", cigarPos, cigarOperation)
 
 		// check if cigarOperation is M (Match) or Mismatch(X)
 		matchMismatchOps := map[string]bool{"M": true, "X": true}
-		_, okMatchMismatch := matchMismatchOps[cigarOperation]
+		_, okMatchMismatchOps := matchMismatchOps[cigarOperation]
 
 		// check if cigarOperation is Insertion (I) or Softclip (S)
 		insertionSoftclipOps := map[string]bool{"I": true, "S": true}
@@ -121,17 +127,28 @@ func ConvertCoordinate(query Query, transcripts map[string]Transcript) (string, 
 		deletionHardClipOps := map[string]bool{"D": true, "H": true}
 		_, okDeletionHardClipOps := deletionHardClipOps[cigarOperation]
 
-		if okMatchMismatch {
-			fmt.Printf("its M or X; %v\n", cigarOperation)
+		if okMatchMismatchOps {
+			// VV: checks if the entire transcript query can be within the current cigar operation length
+			if queryCoordinate <= cigarPos {
+				// VV: transcript can be fully aligned with the current operation updating the genomic position
+				finalPos += queryCoordinate
+				break // TODO: check if this breaks the for loop as well
+			} else {
+				// VV: if not the genomic and transcript coord are updated to continue processing subsequent cigar operations
+				finalPos += cigarPos
+				queryCoordinate -= cigarPos
+			}
 		} else if okInsertionSoftclipOps {
-			fmt.Printf("its I or S; %v\n", cigarOperation)
+			// VV: Insertion or soft clip operation are bases added only in transcript query sequence, so cigar length for insertion needs to be deducted from transcript query sequence.
+			queryCoordinate -= cigarPos
 		} else if okDeletionHardClipOps {
-			fmt.Printf("its D or H; %v\n", cigarOperation)
+			// VV: Deletion or hard clip operation are bases in ref genome sequence not in query transcript sequence, so we add cigar length to genomic coordinate.
+			finalPos += cigarPos
 		}
 
 	}
 
-	return "NA", 0, nil
+	return query.Name, finalPos, nil
 }
 
 func main() {
